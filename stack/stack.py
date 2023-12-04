03e36b1bdea51cb5c6fdd75bebfa6d0ab4ae143b
N Digvijay <digvijayn1993@gmail.com>
2023-12-04 11:25:49 +0530

    fixed more errors

--- stack/stack.py
+++ stack/stack.py
@@ -4,13 +4,13 @@ class Stack:
     def push(self,item):
         self.stack.append(item)
     def pop(self):
-        if(self.isempty):
+        if(self.isempty()):
             print("stack is empty")
         else:
             ele=self.stack.pop()
             print("popped..........",ele,"\n")
     def peek(self):
-        if(self.isempty):
+        if(self.isempty()):
             print("stack is empty")
         else:
             ele=self.stack[-1]
@@ -18,7 +18,7 @@ class Stack:
     def print(self):
         print(self.stack)
     def isempty(self):
-        if self.stack==[]:
+        if len(self.stack)==0:
             return True
         return False
     
@@ -27,9 +27,9 @@ class Stack:
 
 if __name__=="__main__":
     stk1=Stack()
-    # stk1.push(5)
-    # stk1.push(10)
-    # stk1.push(15)
+    stk1.push(5)
+    stk1.push(10)
+    stk1.push(15)
     stk1.pop()
     stk1.peek()
     stk1.print()
